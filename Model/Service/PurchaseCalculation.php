83969b33f4f45db6e6b9b2427d81a31f658d11fa
Attila Sági <attila.sagi@itegration.com>
2024-09-16 13:10:35 +0200

    Refactor for speed

--- Model/Service/PurchaseCalculation.php
+++ Model/Service/PurchaseCalculation.php
@@ -11,8 +11,6 @@ namespace Space\ProductPurchaseCount\Model\Service;
 use Space\ProductPurchaseCount\Api\Data\ProductPurchaseCountInterface;
 use Space\ProductPurchaseCount\Api\PurchaseCalculationInterface;
 use Space\ProductPurchaseCount\Api\Data\ProductPurchaseCountInterfaceFactory;
-use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
-use Magento\Sales\Model\ResourceModel\Order\Item\CollectionFactory as OrderItemCollectionFactory;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Sales\Model\ResourceModel\Order\Item as ResourceItem;
 use Magento\Sales\Model\ResourceModel\Order as ResourceOrder;
@@ -31,16 +29,6 @@ class PurchaseCalculation implements PurchaseCalculationInterface
      * @var ProductPurchaseCountInterfaceFactory
      */
     private ProductPurchaseCountInterfaceFactory $productPurchaseCountFactory;
-
-    /**
-     * @var OrderCollectionFactory
-     */
-    private OrderCollectionFactory $orderCollectionFactory;
-
-    /**
-     * @var OrderItemCollectionFactory
-     */
-    private OrderItemCollectionFactory $orderItemCollectionFactory;
 
     /**
      * @var StoreManagerInterface
@@ -81,8 +69,6 @@ class PurchaseCalculation implements PurchaseCalculationInterface
      * Construct
      *
      * @param ProductPurchaseCountInterfaceFactory $productPurchaseCountFactory
-     * @param OrderCollectionFactory $orderCollectionFactory
-     * @param OrderItemCollectionFactory $orderItemCollectionFactory
      * @param StoreManagerInterface $storeManager
      * @param ResourceItem $resourceItem
      * @param ResourceOrder $resourceOrder
@@ -93,8 +79,6 @@ class PurchaseCalculation implements PurchaseCalculationInterface
      */
     public function __construct(
         ProductPurchaseCountInterfaceFactory $productPurchaseCountFactory,
-        OrderCollectionFactory $orderCollectionFactory,
-        OrderItemCollectionFactory $orderItemCollectionFactory,
         StoreManagerInterface $storeManager,
         ResourceItem $resourceItem,
         ResourceOrder $resourceOrder,
@@ -104,8 +88,6 @@ class PurchaseCalculation implements PurchaseCalculationInterface
         LoggerInterface $logger
     ) {
         $this->productPurchaseCountFactory = $productPurchaseCountFactory;
-        $this->orderCollectionFactory = $orderCollectionFactory;
-        $this->orderItemCollectionFactory = $orderItemCollectionFactory;
         $this->storeManager = $storeManager;
         $this->resourceItem = $resourceItem;
         $this->resourceOrder = $resourceOrder;
@@ -123,11 +105,9 @@ class PurchaseCalculation implements PurchaseCalculationInterface
      */
     public function getPurchaseCount(int $productId): ProductPurchaseCountInterface
     {
-        $productPurchaseCountOriginal = $this->productPurchaseCountFactory->create();
+        $productPurchaseCount = $this->productPurchaseCountFactory->create();
         try {
-            //-------------------
-
-            $storeId = $this->storeManager->getStore()->getId();
+            $storeId = (int)$this->storeManager->getStore()->getId();
             $endDate = $this->dateTime->date(StdlibDateTime::DATE_PHP_FORMAT . ' 23:59:59');
             $startDate = $this->dateTime->date(
                 StdlibDateTime::DATE_PHP_FORMAT,
@@ -135,75 +115,22 @@ class PurchaseCalculation implements PurchaseCalculationInterface
             );
             $startDate .= ' 00:00:00';
 
-            //-------------------
-
-            $timeStart = microtime(true);
-            $this->logger->debug('Space getOrdersCount Collection');
-
-            $orderIds = $this->getOrderIdsByProductIdCollection($productId, (int)$storeId, $startDate, $endDate);
-
+            $orderIds = $this->fetchOrderIdsByProductId($productId, $storeId, $startDate, $endDate);
             if (!empty($orderIds)) {
-                $orderCollection = $this->orderCollectionFactory->create();
-                $orderCollection->addAttributeToSelect(OrderInterface::CUSTOMER_EMAIL)
-                    ->addFieldToFilter(OrderInterface::ENTITY_ID, ['in' => $orderIds])
-                    ->addFieldToFilter(OrderInterface::STORE_ID, ['eq' => $storeId]);
-                $orderCollection->getSelect()
-                    ->distinct()
-                    ->group(OrderInterface::CUSTOMER_EMAIL);
-
-                $this->logger->debug((string)$orderCollection->getSelect());
-
-                $productPurchaseCountOriginal->setCount($orderCollection->getSize());
-            } else {
-                $productPurchaseCountOriginal->setCount(0);
-            }
-
-            $timeEnd = microtime(true);
-            $executionTime = $timeEnd - $timeStart;
-
-            $this->logger->debug('Collection count: ' . $productPurchaseCountOriginal->getCount());
-            $this->logger->debug('From Date: ' . $startDate);
-            $this->logger->debug('To Date: ' . $endDate);
-            $this->logger->debug('Store Id: ' . $storeId);
-            $this->logger->debug('Time: ' . $executionTime);
-            $this->logger->debug('-----------');
-
-            //-------------------
-
-            $timeStart = microtime(true);
-
-            $this->logger->debug('Space getOrdersCount API Direct');
-
-            $productPurchaseCount = $this->productPurchaseCountFactory->create();
-            $orderIds = $this->fetchOrderIdsByProductId($productId, (int)$storeId, $startDate, $endDate);
-
-            if (!empty($orderIds)) {
-                $orderCount = $this->fetchOrdersCountByOrderIds(array_unique($orderIds), (int)$storeId);
+                $orderCount = $this->fetchOrdersCountByOrderIds(array_unique($orderIds), $storeId);
                 $productPurchaseCount->setCount($orderCount);
                 $productPurchaseCount->setNotificationText($this->convertNotificationText($orderCount));
             } else {
                 $productPurchaseCount->setCount(0);
                 $productPurchaseCount->setNotificationText('');
             }
-
-            $timeEnd = microtime(true);
-            $executionTime = $timeEnd - $timeStart;
-
-            $this->logger->debug('Direct count: ' . $productPurchaseCount->getCount());
-            $this->logger->debug('From Date: ' . $startDate);
-            $this->logger->debug('To Date: ' . $endDate);
-            $this->logger->debug('Store Id: ' . $storeId);
-            $this->logger->debug('Time: ' . $executionTime);
-            $this->logger->debug('-----------');
-
-            return $productPurchaseCount;
         } catch (LocalizedException $e) {
             $this->logger->error($e->getMessage());
         } catch (\Exception $e) {
             $this->logger->critical($e->getMessage());
         }
 
-        return $productPurchaseCountOriginal;
+        return $productPurchaseCount;
     }
 
     /**
@@ -224,8 +151,6 @@ class PurchaseCalculation implements PurchaseCalculationInterface
             )
             ->where(OrderInterface::ENTITY_ID . ' IN (?)', $orderIds)
             ->where('store_id = ?', $storeId);
-
-        $this->logger->debug((string)$select);
 
         return (int)$connection->fetchOne($select);
     }
@@ -254,8 +179,6 @@ class PurchaseCalculation implements PurchaseCalculationInterface
             ->where(OrderItemInterface::CREATED_AT . ' >= ?', $startDate)
             ->where(OrderItemInterface::CREATED_AT . ' <= ?', $endDate);
 
-        $this->logger->debug((string)$select);
-
         return $connection->fetchCol($select);
     }
 
@@ -273,43 +196,5 @@ class PurchaseCalculation implements PurchaseCalculationInterface
         }
 
         return $this->escaper->escapeHtml($notificationText, ['strong']);
-    }
-
-    /**
-     * Get order Ids by product ID collection
-     *
-     * @param int $productId
-     * @param int $storeId
-     * string $startDate,
-     * string $endDate
-     * @param string $startDate
-     * @param string $endDate
-     * @return array
-     */
-    private function getOrderIdsByProductIdCollection(
-        int $productId,
-        int $storeId,
-        string $startDate,
-        string $endDate
-    ): array {
-        $orderIds = [];
-
-        $orderItemCollection = $this->orderItemCollectionFactory->create();
-        $orderItemCollection->addAttributeToSelect(OrderItemInterface::ORDER_ID)
-            ->addFieldToFilter(OrderItemInterface::STORE_ID, ['eq' => $storeId])
-            ->addFieldToFilter(OrderItemInterface::PRODUCT_ID, ['eq' => $productId])
-            ->addFieldToFilter(
-                OrderInterface::CREATED_AT,
-                ['from' => $startDate, 'to' => $endDate]
-            )->distinct(true);
-        if ($orderItemCollection->getSize()) {
-            foreach ($orderItemCollection as $orderItem) {
-                $orderIds[] = $orderItem->getOrderId();
-            }
-        }
-
-        $this->logger->debug((string)$orderItemCollection->getSelect());
-
-        return $orderIds;
     }
 }
